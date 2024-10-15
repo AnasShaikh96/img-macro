c97583af3d2b4a4beaa6978fb39baa7080278f35
Anas Shaikh <shaikhanas246@gmail.com>
2024-10-15 18:54:38 +0530

    improving input form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,186 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 import axios from "axios";
-import imageCompression, { Options } from "browser-image-compression";
 import { resizeImg } from "./utils/resizeImg";
 
 function App() {
+  const inputFileRef = useRef<HTMLInputElement | null>(null);
+
   const [files, setFiles] = useState<File[]>([]);
+  const [disableUploadBtn, setDisableUploadBtn] = useState<boolean>(false);
+  const [disableDownloadBtn, setDisableDownloadBtn] = useState(true);
 
   async function handleMultipleChange(
     event: React.ChangeEvent<HTMLInputElement>
   ) {
     if (event.target.files) {
       const images = Array.from(event.target.files) ?? [];
-      // await Promise.all(images.map((image: File) => imageCompressor(image)));
-
-      // console.log(compressedImages);
-
+      // setDisableUploadBtn(false);
       setFiles(images);
     }
   }
-
-  const imageCompressor = async (data: File) => {
-    const options: Options = {
-      maxSizeMB: 0.25,
-      useWebWorker: true,
-      // fileType: "image/png",
-      // onProgress: (e) => console.log(e),
-    };
-
-    try {
-      const compressImage = await imageCompression(data, options);
-      return compressImage;
-      // console.log(compressImage);
-      // setFiles((prev) => [...prev, compressImage]);
-    } catch (error) {
-      console.log("error", error);
-      throw error;
-    }
-  };
-
-  interface IResizeImageOptions {
-    maxSize: number;
-    file: File;
-  }
-  const resizeImage = (settings: IResizeImageOptions) => {
-    const file = settings.file;
-    const maxSize = settings.maxSize;
-
-    const reader = new FileReader();
-    const image = new Image();
-    const canvas = document.createElement("canvas");
-    const dataURItoBlob = (dataURI: string) => {
-      const bytes =
-        dataURI.split(",")[0].indexOf("base64") >= 0
-          ? atob(dataURI.split(",")[1])
-          : unescape(dataURI.split(",")[1]);
-      const mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
-      const max = bytes.length;
-      const ia = new Uint8Array(max);
-      for (let i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
-      return new Blob([ia], { type: mime });
-    };
-    const resize = () => {
-      let width = image.width;
-      let height = image.height;
-
-      if (width > height) {
-        if (width > maxSize) {
-          height *= maxSize / width;
-          width = maxSize;
-        }
-      } else {
-        if (height > maxSize) {
-          width *= maxSize / height;
-          height = maxSize;
-        }
-      }
-
-      canvas.width = width;
-      canvas.height = height;
-      canvas.getContext("2d")?.drawImage(image, 0, 0, width, height);
-      // const dataUrl = canvas.toDataURL("image/jpeg");
-
-      // console.log(
-      //   canvas.toBlob(function (blob) {
-      //     return blob;
-      //   }, file.type)
-      // );
-
-      let saveBlob;
-
-      canvas.toBlob(function (blob) {
-        saveBlob = blob;
-        // if (blob) {
-        // console.log("blob", blob);
-        // const url = URL.createObjectURL(blob);
-        // image.src = url;
-        // console.log(url);
-        // }
-
-        return blob;
-      }, file.type);
-
-      // console.log("saveBlob", saveBlob);
-
-      return saveBlob;
-
-      // return dataURItoBlob(dataUrl);
-    };
-
-    // return new Promise((ok, no) => {
-    //   if (!file.type.match(/image.*/)) {
-    //     no(new Error("Not an image"));
-    //     console.log("error");
-    //     // return;
-    //   }
-
-    //   reader.onload = (readerEvent: any) => {
-    //     image.onload = () => ok(console.log("115 se", resize()));
-
-    //     image.src = readerEvent.target.result;
-    //   };
-    //   reader.readAsDataURL(file);
-    // });
-
-    return new Promise((resolve, reject) => {
-      if (!file) {
-        reject(new Error("rejected"));
-      }
-
-      reader.readAsDataURL(file);
-      reader.onload = (readerEvent: any) => {
-        image.src = readerEvent.target.result;
-        image.onload = () => resolve(resize());
-      };
-
-      return image;
-
-      // console.log(reader.readAsDataURL(file));
-      // if (file instanceof Blob) {
-      // }
-    });
-  };
 
   async function handleMultipleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const url = "http://localhost:3000/upload";
     const formData = new FormData();
 
-    // await Promise.all(files.map((image: File) => imageCompressor(image))).then(
-    //   (res) => {
-    //     res.forEach((file: File, index: number) => {
-    //       const fle = new File([file], file.name, {
-    //         lastModified: Date.now(),
-    //         type: file.type,
-    //       });
-
-    //       // console.log(fle);
-    //       formData.append(`file${index}`, fle);
-    //     });
-    //   }
-    // );
-
     await Promise.all(
-      files.map(async (image: File) => await resizeImg(image))
-    ).then((res) => {
+      files.map(async (image: File) => (await resizeImg(image)) as File)
+    ).then((res: File[]) => {
       res.forEach((file: File, index: number) => {
         formData.append(`file${index}`, file);
       });
     });
-    // .then((res) => console.log(res));
-
-    // files.forEach((file: File, index: number) => {
-    //   // console.log(file.size, resizeImages(file));
-    //   formData.append(`file${index}`, file);
-    // });
-    // await Promise.all(
-    //   files.map(
-    //     (image: File, index: number) => formData.append(`file${index}`, imageCompressor(image))
-    //     // imageCompressor(image)
-    //   )
-    // );
 
     const config = {
       headers: {
@@ -190,9 +41,14 @@ function App() {
 
     await axios
       .post(url, formData, config)
-      .then((response) => {
-        console.log(response.data);
-        // setUploadedFiles(response.data.files);
+      .then(() => {
+        setFiles([]);
+        setDisableDownloadBtn(false);
+
+        const getInputFile = inputFileRef.current;
+        if (getInputFile) {
+          getInputFile.value = "";
+        }
       })
       .catch((error) => {
         console.error("Error uploading files: ", error);
@@ -234,12 +90,15 @@ function App() {
       <form onSubmit={handleMultipleSubmit}>
         <h1>React Multiple File Upload</h1>
         <input
+          ref={inputFileRef}
           type="file"
           accept="image/*"
           multiple
           onChange={handleMultipleChange}
         />
-        <button type="submit">Upload</button>
+        <button type="submit" disabled={disableUploadBtn}>
+          Upload
+        </button>
       </form>
 
       <button onClick={HandleClick} type="button">
