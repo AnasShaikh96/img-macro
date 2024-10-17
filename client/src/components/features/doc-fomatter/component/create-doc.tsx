b39e399eb92b4895fdeb85906ff91d5cdaeac17b
Anas Shaikh <shaikhanas246@gmail.com>
2024-10-17 22:44:22 +0530

    rnd zod and react-hook-form

--- client/src/components/features/doc-fomatter/component/create-doc.tsx
+++ client/src/components/features/doc-fomatter/component/create-doc.tsx
@@ -1,13 +1,46 @@
-import { zodResolver } from "@hookform/resolvers/zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import * as z from "zod";
+// import { Input } from "../../../ui/form";
+
+// const createDocSchema = z.object({
+//   name: z.string().min(1, { message: "sadasd" }),
+//   age: z.number().min(10, { message: "sad" }),
+// });
+
+// export const CreateDoc = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm({
+//     resolver: zodResolver(createDocSchema),
+//   });
+
+//   console.log(errors);
+
+//   return (
+//     <form onSubmit={handleSubmit((d) => console.log(d))}>
+//       {/* <Input error={errors.name?.message?.toString()} /> */}
+//       {/* <input {...register("name")} /> */}
+//       {/* {errors.name?.message && <p>{errors.name?.message ?? ""}</p>} */}
+//       <input type="number" {...register("age", { valueAsNumber: true })} />
+//       {errors.age?.message && <p>{String(errors.age?.message)}</p>}
+//       {/* <Input type="number" error={errors} /> */}
+
+//       <input type="submit" />
+//     </form>
+//   );
+// };
+
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
-const createDocSchema = z.object({
-  name: z.string().min(1, { message: "Required" }),
-  age: z.number().min(10, { message: "Number Required" }),
+const schema = z.object({
+  name: z.string().min(1, { message: "asddddd" }),
+  age: z.number().min(10),
 });
-
-export type CreateDocSchema = z.infer<typeof createDocSchema>;
 
 export const CreateDoc = () => {
   const {
@@ -15,15 +48,15 @@ export const CreateDoc = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: zodResolver(createDocSchema),
+    resolver: zodResolver(schema),
   });
- 
+
   return (
     <form onSubmit={handleSubmit((d) => console.log(d))}>
       <input {...register("name")} />
-      {errors.name?.message && <p>{String(errors.name?.message)}</p>}
+      {errors.name?.message && <p>{errors.name?.message}</p>}
       <input type="number" {...register("age", { valueAsNumber: true })} />
-      {errors.age?.message && <p>{String(errors.age?.message)}</p>}
+      {errors.age?.message && <p>{errors.age?.message}</p>}
       <input type="submit" />
     </form>
   );
--- client/src/components/ui/form/input.tsx
+++ client/src/components/ui/form/input.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { FieldError, FieldErrorsImpl, Merge } from "react-hook-form";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
+  error?: string | undefined;
+  // error?: FieldError | Merge<FieldError, FieldErrorsImpl> | undefined;
 };
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type }, ref) => {
-    return <input className={className} type={type} ref={ref} />;
+  ({ className, type, error }, ref) => {
+    console.log(error);
+    return (
+      <div>
+        <input className={className} type={type} ref={ref} />
+        <p>{error ?? ""}</p>
+      </div>
+    );
   }
 );
 
