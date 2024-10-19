6ad00c2ada36be29a3cf0c1f7d72ebaf47ba3932
Anas Shaikh <shaikhanas246@gmail.com>
2024-10-19 19:47:12 +0530

    removed doc form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,109 +1,89 @@
-import React, { useRef, useState } from "react";
+// import React, { useRef, useState } from "react";
 import "./App.css";
-import axios from "axios";
-import { resizeImg } from "./components/utils/resizeImg";
+// import axios from "axios";
+// import { resizeImg } from "./components/utils/resizeImg";
 import { CreateDoc } from "./components/features/doc-fomatter/component/create-doc";
 
 function App() {
-  const inputFileRef = useRef<HTMLInputElement | null>(null);
+  // const inputFileRef = useRef<HTMLInputElement | null>(null);
 
-  const [files, setFiles] = useState<File[]>([]);
-  // const [disableUploadBtn, setDisableUploadBtn] = useState<boolean>(false);
-  // const [disableDownloadBtn, setDisableDownloadBtn] = useState<boolean>(true);
+  // const [files, setFiles] = useState<File[]>([]);
 
-  async function handleMultipleChange(
-    event: React.ChangeEvent<HTMLInputElement>
-  ) {
-    if (event.target.files) {
-      const images = Array.from(event.target.files) ?? [];
-      // setDisableUploadBtn(false);
-      setFiles(images);
-    }
-  }
+  // async function handleFileChanges(event: React.ChangeEvent<HTMLInputElement>) {
+  //   if (event.target.files) {
+  //     const images = Array.from(event.target.files) ?? [];
+  //     setFiles(images);
+  //   }
+  // }
 
-  async function handleMultipleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const url = "http://localhost:3000/upload";
-    const formData = new FormData();
+  // async function handleUpload(event: React.FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   const url = "http://localhost:3000/upload";
+  //   const formData = new FormData();
 
-    await Promise.all(
-      files.map(async (image: File) => (await resizeImg(image)) as File)
-    ).then((res: File[]) => {
-      res.forEach((file: File, index: number) => {
-        formData.append(`file${index}`, file);
-      });
-    });
+  //   await Promise.all(
+  //     files.map(async (image: File) => (await resizeImg(image)) as File)
+  //   ).then((res: File[]) => {
+  //     res.forEach((file: File, index: number) => {
+  //       formData.append(`file${index}`, file);
+  //     });
+  //   });
 
-    const config = {
-      headers: {
-        "content-type": "multipart/form-data",
-      },
-    };
+  //   const config = {
+  //     headers: {
+  //       "content-type": "multipart/form-data",
+  //     },
+  //   };
 
-    await axios
-      .post(url, formData, config)
-      .then(() => {
-        setFiles([]);
-        // setDisableDownloadBtn(false);
+  //   await axios
+  //     .post(url, formData, config)
+  //     .then(() => {
+  //       setFiles([]);
+  //       // setDisableDownloadBtn(false);
 
-        const getInputFile = inputFileRef.current;
-        if (getInputFile) {
-          getInputFile.value = "";
-        }
-      })
-      .catch((error) => {
-        console.error("Error uploading files: ", error);
-      });
-  }
+  //       const getInputFile = inputFileRef.current;
+  //       if (getInputFile) {
+  //         getInputFile.value = "";
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error uploading files: ", error);
+  //     });
+  // }
 
-  const HandleClick = async () => {
-    try {
-      await axios
-        .get("http://localhost:3000/download", {
-          responseType: "blob",
-        })
-        .then(async (res) => {
-          try {
-            const blob = await res.data;
+  // const HandleDownload = async () => {
+  //   try {
+  //     await axios
+  //       .get("http://localhost:3000/download", {
+  //         responseType: "blob",
+  //       })
+  //       .then(async (res) => {
+  //         try {
+  //           const blob = await res.data;
 
-            const link = document.createElement("a");
-            const url = window.URL.createObjectURL(blob);
+  //           const link = document.createElement("a");
+  //           const url = window.URL.createObjectURL(blob);
 
-            link.href = url;
-            link.download = "MyDoc.docx";
+  //           link.href = url;
+  //           link.download = "MyDoc.docx";
 
-            document.body.appendChild(link);
-            link.click();
+  //           document.body.appendChild(link);
+  //           link.click();
 
-            window.URL.revokeObjectURL(url);
-            document.body.removeChild(link);
-          } catch (error) {
-            console.log(error);
-          }
-        });
-    } catch (error) {
-      console.error("Error downloading document:", error);
-    }
-  };
+  //           window.URL.revokeObjectURL(url);
+  //           document.body.removeChild(link);
+  //         } catch (error) {
+  //           console.log(error);
+  //         }
+  //       });
+  //   } catch (error) {
+  //     console.error("Error downloading document:", error);
+  //   }
+  // };
 
   return (
     <div className="App">
       <CreateDoc />
-      {/* <form onSubmit={handleMultipleSubmit}>
-        <h1>React Multiple File Upload</h1>
-        <input
-          ref={inputFileRef}
-          type="file"
-          accept="image/*"
-          multiple
-          onChange={handleMultipleChange}
-        />
-        <button type="submit">Upload</button>
-      </form>
-
-      <button onClick={HandleClick} type="button">
-        Dowload Files
-      </button> */}
     </div>
   );
 }
