d461541963296c7de1b9bad36f48ce1af548893f
Anas Shaikh <shaikhanas246@gmail.com>
2025-06-21 13:20:08 +0530

    feat:shifting logic to client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import { CreateDoc } from "./components/features/doc-fomatter/component/create-doc";
+import CreateClientDoc from "./components/features/doc-fomatter/client-component/createDoc";
+// import { CreateDoc } from "./components/features/doc-fomatter/component/create-doc";
 
 function App() {
   return (
     <div className="App">
-      <CreateDoc />
+      <CreateClientDoc />
+      {/* <CreateDoc /> */}
     </div>
   );
 }
